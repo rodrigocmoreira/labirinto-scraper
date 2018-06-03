e0f3583a9979de2fcd527ede8841cd7ceba73408
rodrigocmoreira <rcardoso.moreira@gmail.com>
2018-06-03 11:08:52 +0200

    adding route to get shows and cast

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # tvmaze-scraper
+
+To test on windows use:
+
+npm run test_windows
+or
+npm run mocha_windows
+
+on Unix based os systems use:
+
+npm test
+or
+npm run mocha
--- test/integration/shows-cast/shows-cast.test.js
+++ test/integration/shows-cast/shows-cast.test.js
@@ -1,23 +1,54 @@
-const ROOT_PATH = process.cwd()
 const async = require('async');
-const Chance = require('chance');
-const chance = new Chance();
 const supertest = require('supertest');
 const { assert } = require('chai');
-const { config } = require(`${ROOT_PATH}/lib/commons`);
-const server = require(`${ROOT_PATH}/lib/application`);
-const url = require('url');
+const { mongoService } = require('../../../lib/repository');
+const server = require('../../../lib/application');
 
 let app;
 
 describe('Integration test shows-cast', () => {
   describe('# /showsCast/ route', () => {
+    const shows = [
+      {
+        id: 1,
+        name: 'Game of Thrones',
+        cast: [
+          {
+            id: 9,
+            name: 'Dean Norris',
+            birthday: new Date('1963-04-08')
+          },
+          {
+            id: 7,
+            name: 'Mike Vogel',
+            birthday: new Date('1979-07-17')
+          },
+        ]
+      },
+      {
+        id: 4,
+        name: 'Big Bang Theory',
+        cast: [
+          {
+            id: 6,
+            name: 'Michael Emerson',
+            birthday: new Date('1950-01-01')
+          },
+        ]
+      }
+    ];
+
     before((done) => {
       async.series([
         (callback) => {
           server.start((err, express) => {
             app = express;
             callback();
+          });
+        },
+        (callback) => {
+          mongoService.getMongoInstance((err, mongo) => {
+            mongo.addManyShows(shows, 'shows', callback);
           });
         }
       ], (err) => {
@@ -30,9 +61,35 @@ describe('Integration test shows-cast', () => {
     });
 
     it('Should return 200 and a list of shows and Cast', (done) => {
-      const expectedBodyResult = {
-        shows: []
-      };
+      const expectedBodyResult = [
+        {
+          id: 1,
+          name: 'Game of Thrones',
+          cast: [
+            {
+              id: 9,
+              name: 'Dean Norris',
+              birthday: new Date('1963-04-08').toISOString()
+            },
+            {
+              id: 7,
+              name: 'Mike Vogel',
+              birthday: new Date('1979-07-17').toISOString()
+            },
+          ]
+        },
+        {
+          id: 4,
+          name: 'Big Bang Theory',
+          cast: [
+            {
+              id: 6,
+              name: 'Michael Emerson',
+              birthday: new Date('1950-01-01').toISOString()
+            },
+          ]
+        }
+      ];
 
       supertest(app)
         .get('/showsCast/')
