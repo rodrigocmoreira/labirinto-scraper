980c9b19a81be07b2cf0d272d5ce59a747ea3139
rodrigocmoreira <rcardoso.moreira@gmail.com>
2018-08-27 17:25:35 +0200

    Replacing callbacks with async and await on the save shows module

--- test/unit/scraper/save-shows.test.js
+++ test/unit/scraper/save-shows.test.js
@@ -1,44 +1,44 @@
-// const sinon = require('sinon');
-// const winston = require('winston');
-// const { assert } = require('chai');
-// const { dbService } = require('../../../lib/repository');
-// const { saveAll } = require('../../../lib/scraper/save-shows');
+const sinon = require('sinon');
+const winston = require('winston');
+const { assert } = require('chai');
+const { dbService } = require('../../../lib/repository');
+const { saveAll } = require('../../../lib/scraper/save-shows');
 
-// describe('Unit Test save-shows', () => {
-//   let sandbox;
+describe.only('Unit Test save-shows', () => {
+  let sandbox;
 
-//   beforeEach(() => {
-//     sandbox = sinon.sandbox.create();
-//     sandbox.spy(winston, 'error');
-//     sandbox.spy(winston, 'info');
-//   });
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+    sandbox.spy(winston, 'error');
+    sandbox.spy(winston, 'info');
+  });
 
-//   afterEach(() => {
-//     sandbox.restore();
-//   });
+  afterEach(() => {
+    sandbox.restore();
+  });
 
-//   it('Should receive an error on db AddMany', (done) => {
-//     const errorOnAddMany = {
-//       message: 'error adding many'
-//     };
+  it('Should receive an error on db AddMany', async () => {
+    const errorOnAddMany = {
+      message: 'error adding many'
+    };
 
-//     const mongoFakeObject = {
-//       addMany: (shows, callback) => callback(errorOnAddMany)
-//     };
+    const mongoFakeObject = {
+      addMany: () => Promise.reject(errorOnAddMany)
+    };
 
-//     sandbox.stub(dbService, 'getDBInstance')
-//       .yields(null, mongoFakeObject, {});
+    sandbox.stub(dbService, 'getDBInstance')
+      .resolves(mongoFakeObject);
 
-//     const shows = [{
-//       id: 1
-//     }];
+    const shows = [{
+      id: 1
+    }];
 
-//     saveAll(shows, (err, result) => {
-//       assert.deepEqual(err.message, errorOnAddMany.message);
-//       assert.deepEqual(result, shows);
-//       sinon.assert.calledOnce(dbService.getDBInstance);
-//       sinon.assert.calledOnce(winston.error);
-//       done();
-//     });
-//   });
-// });
+    try {
+      await saveAll(shows);
+    } catch (error) {
+      assert.deepEqual(error.message, errorOnAddMany.message);
+      sinon.assert.calledOnce(dbService.getDBInstance);
+      sinon.assert.calledOnce(winston.error);
+    }
+  });
+});
